d493ecfe2f629db147660d11fb99594727705f74
Michael Orwin <michaelxorwin@gmail.com>
2018-10-12 14:52:46 -0400

    update karma-launchers

--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -1,17 +1,33 @@
+//function add1(a) {
+//  return a + 1;
+//}
+
+//var s = sinon.spy(add1);
+//add1(1);
+//expect(true).to.be.true();
+
 describe('phoneList', function() {
   var PhoneListController;
+  //spy($componentController,'phoneList');
 
   beforeEach(function () {
     module('phoneList')
-
+    //var spy = sinon.spy();
+    //console.debug(spy);
     inject(function ($componentController) {
-        PhoneListController = $componentController('phoneList');
+      //console.debug(sinon.spy());
+      PhoneListController = $componentController('phoneList');
+      //$componentController.phoneList.restore();
     });
+  });
+  afterEach(function () {
+    //console.debug(spy.thisValues());
   });
 
   describe('PhoneListController', function() {
     it('should create a `phones` model with 3 phones', function() {
       PhoneListController.phones.length.should.equal(3);
+      //expect(true).to.be.true();
     });
   });
 });
