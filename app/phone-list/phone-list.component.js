cf9ee110d7a9761cfa8b094678bfa7b4163f604d
Michael Orwin <michaelxorwin@gmail.com>
2018-10-12 17:01:28 -0400

    7 - XHR & Dependency Injection

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,23 +1,16 @@
+// The ['http'] is not neccessary but fixes minification
 angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: function PhoneListController() {
-      this.phones = [
-        {
-          name: 'Nexus S',
-          snippet: 'Fast just got faster with Nexus S.',
-          age: 1
-        }, {
-          name: 'Motorola XOOM™ with Wi-Fi',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 2
-        }, {
-          name: 'MOTOROLA XOOM™',
-          snippet: 'The Next, Next Generation tablet.',
-          age: 3
-        }
-      ];
-      this.orderProp = 'age';
-    }
+    controller: ['$http',
+      function PhoneListController($http) {
+        this.orderProp = 'age';
+
+        var self = this;
+        $http.get('phones/phones.json').then(function(response) {
+          self.phones = response.data.slice(0, 10);
+        });
+      }
+    ]
   });
--- app/phone-list/phone-list.component.spec.js
+++ app/phone-list/phone-list.component.spec.js
@@ -4,14 +4,24 @@ describe('phoneList', function() {
   beforeEach(function () {
     module('phoneList')
 
-    inject(function ($componentController) {
+    inject(function ($componentController, _$httpBackend_) {
+      // The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
+      // This allows us to inject a service and assign it to a variable with the same name
+      // as the service while avoiding a name conflict.
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/phones.json')
+                  .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+
       PhoneListController = $componentController('phoneList');
     });
   });
 
   describe('PhoneListController', function() {
-    it('should create a `phones` model with 3 phones', function() {
-      PhoneListController.phones.length.should.equal(3);
+    it('should create a `phones` property with 2 phones fetched with `$http`', function() {
+      should.not.exist(PhoneListController.phones);
+
+      $httpBackend.flush();
+      PhoneListController.phones.should.deep.equal([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
     });
     it('should set a default value for the `orderProp` model', function() {
       PhoneListController.orderProp.should.equal('age');
